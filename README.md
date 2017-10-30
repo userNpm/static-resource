f853a84cf5e9229316276ea40e8abec831700b7e
qinhailang <qhl@qinhailangdeMac-mini.local>
2017-10-30 17:41:49 +0800

    1.00

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # static-resource
 静态资源管理
+
+## 第一步：安装工具
+
+这个命令行小工具依赖 [nodejs](http://nodejs.org/) 环境，因此，请先确保本地安装了它。
+
+使用 [nodejs](http://nodejs.org/) 随带的 [npm](https://www.npmjs.org/) 包管理工具进行安装：
+
+```bash
+npm install -g rsd
+```
+
+## 第二步：创建项目
+
+```bash
+mkdir rsd-project
+cd rsd-project
+```
+
+在项目根目录下创建一个空的 ``fis-conf.js`` 文件，空着就行。
+
+然后开始在项目目录下，建自己的文件目录！
+
+## 第三步：发布代码
+
+在项目根目录下执行：
+
+```bash
+rsd release --md5 --dest ../output
+```
+
+然后去到 ``../output`` 目录下去查看一下产出结果吧，所有静态资源都以md5摘要形式发布了出来，所有资源链接，我说 **所有链接**，包括html中的图片、样式、脚本以及js中的资源地址、css中的资源地址全部都加上了md5戳。
+
+上述命令中，``--md5`` 就是表示要给所有资源定位标记加上摘要信息的意思，不加这个参数就没有摘要信息处理。``--dest ../output`` 表示把代码发布到当前目录上一级的output目录中。整个这条命令还可以简写成：
+
+```bash
+rsd release -md ../output
+```
+
+## 把代码发布指定位置：
+
+```bash
+rsd release -md d:\wwwroot
+```
+
+如果你本地没有安装任何服务器，你可以使用rsd内置的调试服务器，执行命令：
+
+```bash
+rsd server start
+```
+
+接下来我们同样要把代码发布到这个内置服务器中，release命令如果省略 ``--dest <path>``参数，就表示把代码发布到内置服务器的根目录下：
+
+```bash
+rsd release -m
+```
+
+在浏览器中访问： http://localhost:8080 即可
---
+++ index.js
@@ -0,0 +1,56 @@
+var fis = module.exports = require('fis');
+fis.require.prefixes = [ 'rsd', 'scrat', 'fis' ];
+fis.cli.name = 'rsd';
+fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
+
+fis.cli.help.commands = [ 'release', 'server' ];
+
+fis.config.set('roadmap.ext', {
+    less: 'css',
+    sass: 'css',
+    scss: 'css',
+    styl: 'css',
+    stylus: 'css',
+    coffee: 'js',
+    md: 'html',
+    jade: 'html',
+    ejs: 'js',
+    handlebars: 'js'
+});
+
+fis.config.set('modules.parser', {
+    less: 'less',
+    sass: 'node-sass',
+    scss: 'node-sass',
+    styl: 'stylus',
+    stylus: 'stylus',
+    coffee: 'coffee-script',
+    md: 'marked',
+    jade: 'jade',
+    ejs: 'ejs',
+    handlebars: 'handlebars'
+});
+
+fis.config.set('modules.optimizer.html', 'html-minifier');
+fis.config.set('settings.optimizer.png-compressor.type', 'pngquant');
+fis.config.set('settings.optimizer.html-minifier', {
+    removeComments: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    removeAttributeQuotes: true
+});
+
+fis.config.set('roadmap.path', [
+    {
+        reg: '**.ejs',
+        release: false
+    },
+    {
+        reg: '**.handlebars',
+        release: false
+    },
+    {
+        reg: /\/_[^\/]+$/,
+        release: false
+    }
+]);
